5c4a2c6e187ba22615238169ba1bca9caafc6f44
DorisWang0117 <wan9jiayi@gmail.com>
2021-03-19 15:53:11 -0700

    update

---
+++ Mosaic/src/test/Config.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Config {
+
+	//the path for those small imgs
+	public static final String MOSAIC_PROCESS_IMG = "img/";
+	
+	public static final int IMG_WIDTH = 32;
+    public static final int IMG_HEIGHT = 32;
+}
